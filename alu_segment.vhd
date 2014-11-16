b04609009f4dc9bb256c2283d349c028b5473a47
Bruno Peynetti <bpeynetti@u.northwestern.edu>
2014-11-15 19:39:47 -0600

    Fix alu_segment and registerfile32

--- alu_segment.vhd
+++ alu_segment.vhd
@@ -7,7 +7,7 @@ ENTITY alu_segment IS
 	PORT(
 		busA : in std_logic_vector(31 downto 0);
 		busB : in std_logic_vector(31 downto 0);
-		imm16 : in std_logic_vector(15 downto 0);
+		Imm16 : in std_logic_vector(15 downto 0);
 		--control signals
 		ALUctr: 	in std_logic_vector(3 downto 0);
 		MemWr: 		in std_logic;
@@ -18,17 +18,17 @@ ENTITY alu_segment IS
 		Equal:	 	out std_logic;
 		busW:		out std_logic_vector(31 downto 0);
 		--clock
-		clk : 		in std_logic;
+		clk : 		in std_logic
 	);
-END alu_segment;
+END ENTITY alu_segment;
 
 ARCHITECTURE struct OF alu_segment IS
 
 	SIGNAL ALU_R:	std_logic_vector(31 downto 0);
-	SIGNAL extender:std_logic_vector(31 downto 0);
+	SIGNAL signExtend:std_logic_vector(31 downto 0);
 	SIGNAL bMux:	std_logic_vector(31 downto 0);
 	SIGNAL data_out:std_logic_vector(31 downto 0);
-	SIGNAL data_in: std_logic_vector(31 downto 0);
+	SIGNAL ALU_B:	std_logic_vector(31 downto 0);
 	
 	COMPONENT alu is
 		PORT(
@@ -54,27 +54,12 @@ ARCHITECTURE struct OF alu_segment IS
 	
 	BEGIN 
 	
-	alu_map: alu PORT MAP (A=>busA, B=>busB, ctrl=>ALUctr, cout=>ALU_R, ze=>Equal);
-	mux1_map: mux_32 PORT MAP (src0=>)
+	alu_map: 	alu PORT MAP (A=>busA, B=>busB, ctrl=>ALUctr, cout=>ALU_R, ze=>Equal);
+	sextender: extender PORT MAP (imm16=>Imm16,ExtOp=>ExtOp,outExt=>signExtend);
+	mux1_map: mux_32 PORT MAP (sel=>ALUSrc,src0=>busB,src1=>signExtend,z=>ALU_B);
+	
+	--memory_map: data_mem PORT MAP (dIn=>busB, clk=>clk, WrEn=>MemWr, Adr=>ALU_R, datOut=>data_out);
+	
+	mux2_map: mux_32 PORT MAP (sel=>MemtoReg,src0=>ALU_R,src1=>data_out,z=>busW);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+END ARCHITECTURE struct;
